4c5fce6d52b9d8dfd4b1c1e65b2b81569c380433
Tatiana Veratti <Tatiana.Veratti@topbox.io>
2020-06-23 14:15:21 -0500

    fix netlify-cms plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,12 +29,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
+    'gatsby-plugin-netlify-cms',
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 };
